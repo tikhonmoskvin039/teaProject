b6cece7f843fcb7ceca77af8fe7309b894ff6a60
Moskvin Tikhon S <tihonmoskvin@mail.ru>
2022-11-30 10:09:14 +0300

    merges

--- views/Private.jsx
+++ views/Private.jsx
@@ -1,16 +1,16 @@
-const { template } = require('@babel/core');
+// const { template } = require('@babel/core');
 const React = require('react');
 const Layout = require('./Layout');
 
 function Private({ comments, username = '' }) {
     return (
-        <Layout>
-            <script defer src="/js/private.js"></script>
-            <div>
-                <h2>Личный кабинет </h2>
-                <h2>Hello, {username}</h2>
-                <div class="row">
-                    {/* <div class="col-4">
+      <Layout username={username}>
+        <script defer src="/js/private.js"></script>
+        <div>
+          <h2>Личный кабинет </h2>
+          <h2>Hello, {username}</h2>
+          <div class="row">
+            {/* <div class="col-4">
                         <div id="list-example" class="list-group">
                             <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
                             <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
@@ -18,20 +18,30 @@ function Private({ comments, username = '' }) {
                             <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
                         </div>
                     </div> */}
-                    <div class="foo" className="row">
-                        {comments[0]["Teas.Comment.text"] ? comments.map((comment, index) => (
-                            <div className="col-sm-6">
-                                <h4 id="scrollspyHeading1">{comment['Teas.name']}</h4>
-                                {/* <p>{comment["Teas.Comment.createdAt"]}</p> */}
-                                <p>{console.log(new Date(comment["Teas.Comment.text"]))}</p>
-                                <p>{comment["Teas.Comment.text"]}</p>
-                                <a href='#' data-delBtn={(index + 1)} class="btn btn-primary btn-sm">Удалить комментарий</a>
-                            </div>
-                        )) : <h3>Список комментариев пуст</h3>}
-                    </div>
-                </div>
+            <div class="foo" className="row">
+              {comments[0]["Teas.Comment.text"] ? (
+                comments.map((comment, index) => (
+                  <div className="col-sm-6">
+                    <h4 id="scrollspyHeading1">{comment["Teas.name"]}</h4>
+                    {/* <p>{comment["Teas.Comment.createdAt"]}</p> */}
+                    <p>{console.log(new Date(comment["Teas.Comment.text"]))}</p>
+                    <p>{comment["Teas.Comment.text"]}</p>
+                    <a
+                      href="#"
+                      data-delBtn={index + 1}
+                      class="btn btn-primary btn-sm"
+                    >
+                      Удалить комментарий
+                    </a>
+                  </div>
+                ))
+              ) : (
+                <h3>Список комментариев пуст</h3>
+              )}
             </div>
-        </Layout>
+          </div>
+        </div>
+      </Layout>
     );
 }
 
