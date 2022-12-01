d5734b906adf6b20ce00d3c392b6791b8192906b
Moskvin Tikhon S <tihonmoskvin@mail.ru>
2022-12-01 20:50:39 +0300

    add: adaptive

--- public/js/client.js
+++ public/js/client.js
@@ -1,36 +1,8 @@
-const allDeleteBtns = document.querySelectorAll("[data-delBtn]");
-
-allDeleteBtns.forEach((del) => {
-  del.addEventListener("click", async (e) => {
-    e.preventDefault();
-    const id = e.target.closest(".card").id;
-    e.target.closest(".card").remove();
-
-    let response;
-    try {
-      response = await fetch("/private/admin", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id,
-        }),
-      });
-      const data = await response.json();
-      console.log("data", data);
-      if (response.status === 200) window.location.href = "/private";
-    } catch (err) {
-      console.log("err");
-    }
-  });
-});
-
-const allUpdateBtns = document.querySelectorAll("[data-updatebtn]");
+const container = document.querySelector(".container.pb-4.pt-3");
 
 const template = `
-   <div class="card card-body">
-            <form id="updForm" method="POST" action="/private/admin">
+   <div class="card card-body edit">
+            <form id="updForm">
               <div class="form-group">
                 <label htmlFor="name" style="width: 100%;">Название чая:</label>
                 <input
@@ -87,47 +59,109 @@ const template = `
               </div>
               <div class="d-flex justify-content-around">
 
-              <button data-Cancel type="button" class="btn btn-outline-danger mt-2" href="/private/admin">
-                Отменить
-              </button>
-              <button data-save type="submit" class="btn btn-outline-warning mt-2" >
-                Сохранить
-              </button>
-              
+              <button data-Cancel type="button" class="btn btn-outline-danger mt-2" href="/private/admin">Отменить</button>
+              <button data-save type="submit" class="btn btn-outline-warning mt-2" >Сохранить</button>
               </div>
             </form>
           </div>
 `;
 
-allUpdateBtns?.forEach((upd) => {
-  upd.addEventListener("click", async (e) => {
-    e.preventDefault();
-    const teaBox = e.target.closest(".card-body");
-    const oldData = teaBox.innerHTML;
-    console.log('teaBox', teaBox)
-    // teaBox.insertAdjacentHTML("afterbegin", template);
-    teaBox.innerHTML = template;
+container.addEventListener("click", async (e) => {
+  const bigTeaBox = e.target.closest(".card");
+  console.log("bigTeaBox", bigTeaBox);
+  const oldData = bigTeaBox.innerHTML;
+  console.log("oldData", oldData);
+  if (e.target.textContent === "Удалить") {
+    const id = e.target.closest(".card").id;
+    e.target.closest(".card").remove();
 
-    const cancelButton = document?.querySelector("[data-Cancel]");
-    cancelButton.addEventListener("click", () => {
-       teaBox.innerHTML = oldData;
-    });
-    const dataSave = document?.querySelector("[data-save]");
-    console.log("dataSave", dataSave);
-    dataSave.addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
+    let response;
+    try {
+      response = await fetch("/private/admin", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
+      const data = await response.json();
+      console.log("data", data);
+    } catch (err) {
+      console.log("err");
+    }
+  } else if (e.target.textContent === "Редактировать") {
+    bigTeaBox.innerHTML = template;
+  }
 
+  const editCard = document.querySelector(".card.card-body.edit");
+  console.log("editCard", editCard);
+
+  editCard.addEventListener("click", async (e) => {
     const updateForm = document.querySelector("#updForm");
-    console.log("updateForm", updateForm);
-    updateForm.addEventListener("submit", async (e) => {
-      e.preventDefault();
-      const id = e.target.closest(".card.content").id;
-      const name = updateForm.name.value;
-      const picture_url = updateForm.picture_url.value;
-      const info = updateForm.info.value;
-      const placeOfBirth = updateForm.placeOfBirth.value;
-      const coordinates = updateForm.coordinates.value;
+    const id = e.target.closest(".card.content").id;
+    const name = updateForm.name.value;
+    const picture_url = updateForm.picture_url.value;
+    const info = updateForm.info.value;
+    const placeOfBirth = updateForm.placeOfBirth.value;
+    const coordinates = updateForm.coordinates.value;
+
+    if (e.target.textContent === "Сохранить") {
+      editCard.innerHTML = `
+        <div
+            class="card content mb-3"
+            style={{
+              maxWidth: "100%",
+              borderRadius: "20px",
+            }}
+            id="${id}"
+          >
+            <div class="row">
+              <div class="col-md-4">
+                <img
+                  src="${picture_url}"
+                  class="allImages img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${name}</h5>
+                  <p class="card-text">${info}</p>
+                  <p class="card-text">
+                    <p>
+                      Место происхождения: <b>${placeOfBirth}</b>
+                    </p>
+                    <p>
+                      Географические координаты: <b>${coordinates}</b>
+                    </p>
+                    <small class="text-muted">
+                      Запись создана: <b>${new Date().toTimeString()}</b>
+                    </small>
+                    <hr />
+                    <small class="text-muted">
+                      Запись редактирована:
+                      <b>${new Date().toTimeString()}</b>
+                    </small>
+                    <div className="d-flex justify-content-around mt-2">
+                      <button
+                        type="button"
+                        class="btn btn-outline-success"
+                        data-updateBtn="${id}"
+                      >Редактировать</button>
+                      <button
+                        type="submit"
+                        data-delBtn="${id}"
+                        class="btn btn-outline-danger"
+                      >Удалить</button>
+                    </div>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+      `;
       let response;
       try {
         response = await fetch("/private/admin", {
@@ -145,16 +179,20 @@ allUpdateBtns?.forEach((upd) => {
           }),
         });
         const data = await response.json();
-        if (response.status === 200) window.location.href = "/private/admin";
       } catch (err) {
-        console.log("err");
+        console.log("err", err);
       }
-    });
+    } else if (e.target.textContent === "Отменить") {
+      const bigTeaBox = e.target.closest(".card.card-body.edit");
+      console.log("bigTeaBox", bigTeaBox);
+
+      const editCard = document.querySelector(".card.card-body.edit");
+      editCard.innerHTML = oldData;
+    }
   });
 });
 
 const newAdminBtn = document.querySelector("[data-newAdmin]");
-console.log("newAdminBtn", newAdminBtn);
 
 newAdminBtn.addEventListener("click", async (e) => {
   e.preventDefault();
@@ -182,5 +220,96 @@ newAdminBtn.addEventListener("click", async (e) => {
     if (response.status === 200) window.location.href = "/private/admin";
   } catch (err) {
     console.log("err");
+  }
+});
+
+const addNewTea = document.querySelector("[data-addNewForm]");
+
+addNewTea.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = document.forms.addNewForm.name.value;
+  const picture_url = document.forms.addNewForm.picture_url.value;
+  const info = document.forms.addNewForm.info.value;
+  const placeOfBirth = document.forms.addNewForm.placeOfBirth.value;
+  const coordinates = document.forms.addNewForm.coordinates.value;
+  let response;
+  try {
+    response = await fetch("/private/admin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        picture_url,
+        info,
+        placeOfBirth,
+        coordinates,
+      }),
+    });
+    const data = await response.json();
+    const container = e.target.closest(".container");
+
+    const template = `
+ <div
+            class="card content mb-3"
+            style={{
+              maxWidth: "100%",
+              borderRadius: "20px",
+            }}
+            id="${data.create.id}"
+          >
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img
+                  src="${data.create.picture_url}"
+                  class="img-fluid rounded-start"
+                  style={{ width: "400px", height: "400px" }}
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${data.create.name}</h5>
+                  <p class="card-text">${data.create.info}</p>
+                  <p class="card-text">
+                    <p>
+                      Место происхождения: <b>${data.create.placeOfBirth}</b>
+                    </p>
+                    <p>
+                      Географические координаты: <b>${data.create.coordinates}</b>
+                    </p>
+                    <small class="text-muted">
+                      Запись создана: <b>${data.create.createdAt}</b>
+                    </small>
+                    <hr />
+                    <small class="text-muted">
+                      Запись редактирована:
+                      <b>${data.create.updatedAt}</b>
+                    </small>
+                    <div class="d-flex justify-content-around mt-2">
+                      <button
+                        type="button"
+                        class="btn btn-outline-success"
+                        data-updateBtn="${data.create.id}"
+                      >Редактировать</button>
+                      <button
+                        type="submit"
+                        data-delBtn="${data.create.id}"
+                        class="btn btn-outline-danger"
+                      >Удалить</button>
+                    </div>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+`;
+    container.insertAdjacentHTML("beforeend", template);
+    const collapse = document.querySelector(".mb-4.collapse");
+    collapse.classList.remove('show')
+    console.log('collapse', collapse)
+  } catch (err) {
+    console.log("err", err);
   }
 });
