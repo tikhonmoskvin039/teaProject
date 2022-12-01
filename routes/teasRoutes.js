3fc1ed98e97c2084da04e326ac2ff17e5a9accf5
Valentin Guschin <112811051+valogus@users.noreply.github.com>
2022-12-01 21:53:32 +0400

    Map/entries done

--- routes/teasRoutes.js
+++ routes/teasRoutes.js
@@ -11,32 +11,60 @@ route.get('/', (req, res) => {
   render(Teas, { title: 'Чай' }, res)
 })
 route.get('/all', async (req, res) => {
-  const teaList = await Tea.findAll({raw:true});
+  const teaList = await Tea.findAll({ raw: true });
   // console.log('......', teaList)
-  res.json({teaList})
+  res.json({ teaList })
 })
 
 route.get('/:id', async (req, res) => {
-  const {id, name, isAdmin} = req.session?.user
+  const { id, name, isAdmin } = req?.session?.user
   const teaId = req.params.id
-  const tea = await Tea.findAll({ where: { id: teaId }, include:[ { model:User}], through:{attributes:{include:[`id`]}}, raw: true })
-  const comments = await Comment.findAll({attributes:{include:[`id`]}, raw:true})
-  console.log(comments)
-  console.log(tea)
-  tea.sort((a, b)=>b["Users.Comment.updatedAt"].getTime() - a["Users.Comment.updatedAt"].getTime())
-render(ShowEntry, { tea, id, name, isAdmin }, res)
+  const tea = await Tea.findAll({ where: { id: teaId }, include: [{ model: User }], raw: true })
+  let comments = await Comment.findAll({ where: { tea_id: teaId, user_id: id }, attributes: { include: [`id`] }, order: [['user_id', 'ASC']], raw: true })
+  let commentsUsers = await Tea.findAll({ where: { id: teaId }, include: { model: User, where: { id: id } }, raw: true })
+  // if(isAdmin){
+  //   comments = await Comment.findAll({ where :{ tea_id: teaId }, attributes: { include: [`id`] }, order:[['user_id', 'ASC']], raw: true })
+  //   commentsUsers = await Tea.findAll({ where: { id: teaId}, include: { model: User }, raw: true })
+  //   console.log(comments,commentsUsers)
+  // }
+  // console.log(comments)
+  // console.log(commentsUsers)
+  commentsUsers.forEach((el, index) => {
+    el.commentId = comments[index].id
+  })
+  // console.log(commentsUsers)
+  tea.sort((a, b) => b["Users.Comment.updatedAt"].getTime() - a["Users.Comment.updatedAt"].getTime())
+  render(ShowEntry, { tea, id, name, isAdmin, commentsUsers }, res)
 })
+
+
 route.post('/:id', async (req, res) => {
   // console.log(res.locals.username)
   const data = req.session?.user
-  const {id, name, isAdmin} = req.session?.user
-  const teaId =  req.params.id
-  const { text} =  req.body
-  const newEntry = await Comment.create({user_id:id, tea_id:teaId, text})
-  console.log(newEntry)
-  console.log(teaId,text )
+  const { id, name, isAdmin } = req.session?.user
+  const teaId = req.params.id
+  const { text } = req.body
+  let comments = await Comment.findAll({ attributes: { include: [`id`] }, raw: true })
+  const commentId = comments[comments.length - 1].id + 1;
+  //console.log(comments)
+  data.commentId = commentId;
+  const newEntry = await Comment.create({ id: commentId, user_id: id, tea_id: teaId, text, })
+  //console.log('.........',newEntry)
+  // console.log(teaId, text)
   //const comment = await Comment.create()
-  res.json( data );
+  res.json(data);
+})
+route.put('/:id', async (req, res) => {
+  const { changeMessage, messageId } = req.body
+  const message = await Comment.update( { text: changeMessage }, {where: { id: messageId }});
+// console.log(message)
+// console.log(changeMessage, messageId)
+const data = req.session?.user
+res.json( data );
+ })
+route.delete('/:id', async (req, res) => {
+  await Comment.destroy({ where: { id: req.params.id } });
+  res.json({ status: 200, text: 'all good' });
 })
 
 
--- views/ShowEntry.jsx
+++ views/ShowEntry.jsx
@@ -2,7 +2,10 @@ const e = require('express');
 const React = require('react');
 const Layout = require('./Layout');
 
-function ShowEntry({ tea, id, name, isAdmin, username  }) {
+function ShowEntry({ tea, id, name, isAdmin, username, commentsUsers }) {
+  commentsUsers.reverse()
+  // console.log(commentsUsers)
+  let i = 0
   return (
     <Layout username={username}>
       <script defer src="/js/showentry.js"></script>
@@ -25,7 +28,7 @@ function ShowEntry({ tea, id, name, isAdmin, username  }) {
           <div className='description'>
             <div className="col-md-8">
               <div className="card-body">
-                <h5 class="card-title tea-name">{tea[0].name}</h5>
+                <h5 id={tea[0].id} class="card-title tea-name">{tea[0].name}</h5>
               </div>
               <div className="imgDiv"><img src={tea[0].picture_url} alt={tea[0].name} class="img-fluid rounded-start"></img></div>
               <br />
@@ -34,7 +37,7 @@ function ShowEntry({ tea, id, name, isAdmin, username  }) {
               <div><p>Страна происхождения:<b>{tea[0].placeOfBirth}</b></p></div>
               <br />
 
-             {username? (<div id="entryButton">
+              {username ? (<div id="entryButton">
                 <button
                   class="btn btn-outline-success"
                   value="add"
@@ -43,7 +46,7 @@ function ShowEntry({ tea, id, name, isAdmin, username  }) {
                 >
                   Оставить комментарий
                 </button>
-              </div>) :('') }
+              </div>) : ('')}
               <br />
               <div> Комментарии пользователей:
                 <br />
@@ -56,7 +59,7 @@ function ShowEntry({ tea, id, name, isAdmin, username  }) {
                     </div>
                       <div>
                         <p class="mb-1">{el['Users.Comment.text']}</p>
-                      </div>{(el['Users.id']==id || isAdmin) ?(<button id="delete-button">удалить</button>):('')}</div>)
+                      </div>{(el['Users.id'] == id) ? (<button class="btn btn-outline-success btn-sm m-2" data-change={commentsUsers[i].commentId}>изменить</button>):('')}{(el['Users.id'] == id) ? (<button  class="btn btn-outline-success btn-sm m-2" data-delete={commentsUsers[i ++].commentId}>удалить</button>):('')}</div>)
                   })}
 
 
