d2add1d5e97fddae6f669e8ffe75dad4c56ca873
Valentin Guschin <112811051+valogus@users.noreply.github.com>
2022-12-02 17:49:26 +0400

    Final

--- public/js/private.js
+++ public/js/private.js
@@ -31,28 +31,31 @@ privateDelBtns?.forEach((btn) => {
 
 commentBox.addEventListener('click', async (event) => {
 
-    console.log(event.target.parentNode.parentNode.id)
-    const comment_id = event.target.parentNode.parentNode.id;
+    //console.log(event.target.parentNode.parentNode.id)
+
 
 
     if (event.target.dataset.updbtn) {
-
+        let comment_id ='';
         let text = ""
         if (event.target.parentNode.parentNode.className !== "col-sm-6 comment-box") {
             text = event.target.parentNode.getAttribute('data-updtext');
-            console.log(event.target.parentNode)
+            //console.log(event.target.parentNode)
+            comment_id= event.target.parentNode.id;
         }
         else {
-            console.log("------------", event.target.parentNode)
+            //console.log("------------", event.target.parentNode)
             text = event.target.parentNode.parentNode.getAttribute('data-updtext');
+            comment_id= event.target.parentNode.parentNode.id;
         }
-        console.log(event.target.parentNode.parentNode)
-        console.log(text)
+         //console.log('commentId', comment_id)
+        // console.log(event.target.parentNode.parentNode)
+         //console.log(text)
         event.target.parentNode.innerHTML =
             `
         
         <form data-new=""        
-        data-updbtn=${comment_id}
+        data-idbtn=${comment_id}
         data-updbtntext=${text}>
         <label  name="text" type="text" class="form-label"> комментарий:</label>
         <textarea  class="form-control input" id="text_input" rows="3">${text}</textarea>
@@ -68,13 +71,14 @@ commentBox.addEventListener('click', async (event) => {
 
 
     if (event.target.id === "inputText") {
-        const box = event.target.closest('div').parentNode
+        let box = event.target.closest('div').parentNode
         let text = event.target.closest("div").parentNode.querySelector(".form-control").value
-        const comment_id = event.target.closest("div").parentNode.id
+        const comment_id = event.target.closest("form").dataset.idbtn
+        console.log('------>', comment_id)
         const tea = event.target.closest("div").parentNode.dataset.teaname
-        console.log(comment_id)
-        console.log(text)
-        console.log(box)
+        // console.log(comment_id)
+        // console.log(text)
+        // console.log(box)
 
 
         const response = await fetch('/private/upd', {
@@ -88,15 +92,15 @@ commentBox.addEventListener('click', async (event) => {
             })
         });
         const updInfo = await response.json();
-        console.log("updinfo", updInfo)
+        //console.log("updinfo", updInfo)
         const { tea_id, updatedAt } = updInfo
-
+let count = 0;
         if (updInfo) {
-
+            count+=1;
             event.target.closest("div").parentNode.dataset.updtext = updInfo.text
             //console.log(data)
             console.log(box)
-            box.innerHTML = `
+           if(count===0) {box.innerHTML = `
             <h4 id="scrollspyHeading1">${tea}</h4>
             <p>${new Date(updatedAt).toDateString()}</p>
 
@@ -115,7 +119,28 @@ commentBox.addEventListener('click', async (event) => {
             >
               Удалить комментарий
             </a>
-            `
+            `} else {
+                box = event.target.closest('div')
+                box.innerHTML = `
+            <h4 id="scrollspyHeading1">${tea}</h4>
+            <p>${new Date(updatedAt).toDateString()}</p>
+
+            <p>${updInfo.text}</p>
+            <a
+              href="#"
+              data-updbtn=${comment_id}
+              class="btn btn-outline-success"
+            >
+              Редактировать комментарий
+            </a>
+            <a
+              href="#"
+              data-delBtn=${comment_id}
+              class="btn btn-outline-danger p-1"
+            >
+              Удалить комментарий
+            </a>`              
+            }
         }
     }
 });
